304775f60cdfa480e79bb3d29c781e13e91848d6
Michael Stenta <mike@mstenta.net>
2026-03-19 14:38:25 -0400

    Convert procedural hook implementations to Hook classes with dependency injection.

---
+++ farmos_asset_link/src/Hook/EntityHooks.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\farmos_asset_link\Hook;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Routing\RouteObjectInterface;
+use Drupal\jsonapi\ResourceType\ResourceType;
+
+/**
+ * Entity hook implementations for farmos_asset_link.
+ */
+class EntityHooks {
+
+  public function __construct(
+    protected RouteMatchInterface $routeMatch,
+  ) {}
+
+  /**
+   * Implements hook_entity_create_access().
+   *
+   * Grant JSON:API file upload permissions for entity fields based on
+   * bundle-level permissions. e.g. Allow image uploads for animal assets if the
+   * user has 'create animal asset' permissions.
+   *
+   * TODO: Follow up on https://github.com/farmOS/farmOS/pull/563#issuecomment-1241952618
+   */
+  #[Hook('entity_create_access')]
+  public function entityCreateAccess($account, $context, $entity_bundle) {
+
+    $resource_type = $this->routeMatch->getParameter("resource_type");
+
+    if (!($resource_type instanceof ResourceType)) {
+      return AccessResult::neutral();
+    }
+
+    $entity_type_id = $resource_type->getEntityTypeId();
+    $entity_bundle = $resource_type->getBundle();
+
+    if (empty($entity_type_id) || empty($entity_bundle)) {
+      return AccessResult::neutral();
+    }
+
+    $route = $this->routeMatch->getRouteObject();
+
+    if (empty($route) || $route->getDefault(RouteObjectInterface::CONTROLLER_NAME) != 'jsonapi.file_upload:handleFileUploadForNewResource') {
+      return AccessResult::neutral();
+    }
+
+    return AccessResult::allowedIfHasPermission($account, "create " . $entity_bundle . " " . $entity_type_id);
+  }
+
+}
---
+++ farmos_asset_link/src/Hook/PageHooks.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\farmos_asset_link\Hook;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * Page hook implementations for farmos_asset_link.
+ */
+class PageHooks {
+
+  public function __construct(
+    protected EntityTypeManagerInterface $entityTypeManager,
+  ) {}
+
+  /**
+   * Implements hook_page_attachments().
+   */
+  #[Hook('page_attachments')]
+  public function pageAttachments(array &$attachments) {
+    // TODO: special handling for not-yet-logged-in users
+
+    $storage = $this->entityTypeManager->getStorage('asset_link_default_plugin');
+
+    $ids = $storage->getQuery()
+      ->condition('sidebarUrlPattern', '', '<>')->execute();
+
+    $defaultPluginConfigs = $storage->loadMultiple($ids);
+
+    $sidebarUrlPatterns = [];
+
+    foreach($defaultPluginConfigs as $defaultPluginConfig) {
+      if (!$defaultPluginConfig->status()) {
+        continue;
+      }
+
+      $sidebarUrlPatterns[] = $defaultPluginConfig->sidebarUrlPattern();
+    }
+
+    $attachments['#attached']['library'][] = 'farmos_asset_link/farmos_asset_link_sidecar';
+
+    $attachments['#attached']['drupalSettings']['farmos_asset_link'] = [
+      'sidebar_url_patterns' => $sidebarUrlPatterns,
+    ];
+
+  }
+
+}
