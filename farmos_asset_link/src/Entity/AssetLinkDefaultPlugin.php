dab38314e774c066fb11a96b72b20c7f6f5e80de
Michael Stenta <mike@mstenta.net>
2026-03-25 10:16:27 -0400

    Convert @ConfigEntityType annotation to #[ConfigEntityType] attribute.

--- farmos_asset_link/src/Entity/AssetLinkDefaultPlugin.php
+++ farmos_asset_link/src/Entity/AssetLinkDefaultPlugin.php
@@ -6,59 +6,64 @@ namespace Drupal\farmos_asset_link\Entity;
 
 use Drupal\Core\Config\Entity\ConfigEntityBase;
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
+use Drupal\Core\Entity\Attribute\ConfigEntityType;
+use Drupal\Core\Entity\EntityDeleteForm;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\entity\EntityPermissionProvider;
+use Drupal\entity\Routing\DefaultHtmlRouteProvider;
+use Drupal\farmos_asset_link\Controller\AssetLinkDefaultPluginListBuilder;
+use Drupal\farmos_asset_link\Form\AssetLinkDefaultPluginForm;
 
 /**
  * Defines the AssetLinkDefaultPlugin entity.
- *
- * @ConfigEntityType(
- *   id = "asset_link_default_plugin",
- *   module = "farmos_asset_link",
- *   label = @Translation("Asset Link default plugin"),
- *   label_collection = @Translation("Asset Link default plugins"),
- *   label_singular = @Translation("Asset Link default plugin"),
- *   label_plural = @Translation("Asset Link default plugins"),
- *   label_count = @PluralTranslation(
- *     singular = "@count Asset Link default plugin",
- *     plural = "@count Asset Link default plugins",
- *   ),
- *   handlers = {
- *     "list_builder" = "Drupal\farmos_asset_link\Controller\AssetLinkDefaultPluginListBuilder",
- *     "form" = {
- *       "add" = "Drupal\farmos_asset_link\Form\AssetLinkDefaultPluginForm",
- *       "edit" = "Drupal\farmos_asset_link\Form\AssetLinkDefaultPluginForm",
- *       "delete" = "Drupal\Core\Entity\EntityDeleteForm",
- *     },
- *     "permission_provider" = "\Drupal\entity\EntityPermissionProvider",
- *     "route_provider" = {
- *       "default" = "Drupal\entity\Routing\DefaultHtmlRouteProvider",
- *     },
- *   },
- *   config_prefix = "asset_link_default_plugin",
- *   admin_permission = "administer farm settings",
- *   entity_keys = {
- *     "id" = "id",
- *     "url" = "url",
- *     "sidebarUrlPattern" = "sidebarUrlPattern",
- *     "user_defined" = "user_defined",
- *     "status" = "status",
- *   },
- *   config_export = {
- *     "id",
- *     "url",
- *     "sidebarUrlPattern",
- *     "user_defined",
- *     "status",
- *   },
- *   links = {
- *     "collection" = "/farm/settings/asset_link_default_plugin/list",
- *     "add-form" = "/farm/settings/asset_link_default_plugin/add",
- *     "edit-form" = "/farm/settings/asset_link_default_plugin/{asset_link_default_plugin}/edit",
- *     "delete-form" = "/farm/settings/asset_link_default_plugin/{asset_link_default_plugin}/delete",
- *     "enable" = "/farm/settings/asset_link_default_plugin/{asset_link_default_plugin}/enable",
- *     "disable" = "/farm/settings/asset_link_default_plugin/{asset_link_default_plugin}/disable",
- *   }
- * )
  */
+#[ConfigEntityType(
+  id: 'asset_link_default_plugin',
+  label: new TranslatableMarkup('Asset Link default plugin'),
+  label_collection: new TranslatableMarkup('Asset Link default plugins'),
+  label_singular: new TranslatableMarkup('Asset Link default plugin'),
+  label_plural: new TranslatableMarkup('Asset Link default plugins'),
+  config_prefix: 'asset_link_default_plugin',
+  entity_keys: [
+    'id' => 'id',
+    'url' => 'url',
+    'sidebarUrlPattern' => 'sidebarUrlPattern',
+    'user_defined' => 'user_defined',
+    'status' => 'status',
+  ],
+  handlers: [
+    'list_builder' => AssetLinkDefaultPluginListBuilder::class,
+    'form' => [
+      'add' => AssetLinkDefaultPluginForm::class,
+      'edit' => AssetLinkDefaultPluginForm::class,
+      'delete' => EntityDeleteForm::class,
+    ],
+    'permission_provider' => EntityPermissionProvider::class,
+    'route_provider' => [
+      'default' => DefaultHtmlRouteProvider::class,
+    ],
+  ],
+  links: [
+    'collection' => '/farm/settings/asset_link_default_plugin/list',
+    'add-form' => '/farm/settings/asset_link_default_plugin/add',
+    'edit-form' => '/farm/settings/asset_link_default_plugin/{asset_link_default_plugin}/edit',
+    'delete-form' => '/farm/settings/asset_link_default_plugin/{asset_link_default_plugin}/delete',
+    'enable' => '/farm/settings/asset_link_default_plugin/{asset_link_default_plugin}/enable',
+    'disable' => '/farm/settings/asset_link_default_plugin/{asset_link_default_plugin}/disable',
+  ],
+  admin_permission: 'administer farm settings',
+  label_count: [
+    'singular' => '@count Asset Link default plugin',
+    'plural' => '@count Asset Link default plugins',
+  ],
+  config_export: [
+    'id',
+    'url',
+    'sidebarUrlPattern',
+    'user_defined',
+    'status',
+  ],
+)]
 class AssetLinkDefaultPlugin extends ConfigEntityBase implements ConfigEntityInterface {
 
   /**
